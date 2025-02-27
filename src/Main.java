a02c6a2d7e48bdf9e81bf20439b28b44dbb2da36
hieupv <hieupv2412@gmail.com>
2025-02-27 23:37:27 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,52 @@
-import java.util.Arrays;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//        String name = "hieupv";
-//
-//        int length = name.length();
-//        char letter = name.charAt(0);
-//
-//        System.out.println(length);
-//        System.out.println(letter);
-//
-//        String email = "hieupv@gmail.com";
-//
-//        System.out.println(email.substring(0, email.indexOf("@")));
-//        System.out.println(email.substring(email.indexOf("@")));
-//
-//        String day = "day";
-//        switch (day) {
-//            case "day" -> System.out.println("Day");
-//        }
+//        How to Play Audio with Java (.wav, .au, .aiff)
 
-        // Array
-        String[] fruits = {"apple", "orange", "banana", "coconut"};
+        String filePath = "src/What If So Then - Karneef.wav";
+        File file = new File(filePath);
 
-        Arrays.sort(fruits);
+        try (Scanner scanner = new Scanner(System.in)) {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
 
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+            String response = "";
+
+            while (!response.equals("Q")) {
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Restart");
+                System.out.println("Q = Quit");
+                System.out.println("Enter your choise: ");
+
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file is not supported");
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+        } catch (LineUnavailableException e) {
+            System.out.println("Unable to access audio resource");
+        } finally {
+            System.out.println("Done");
         }
-
-        String[] foods = new String[1];
-        foods[0] = "pizza";
-        System.out.println(Arrays.toString(foods));
     }
 }
