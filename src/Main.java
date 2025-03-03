037f2b762aba612e1b7383b82a1d30b98a3997c4
hieupv <hieupv2412@gmaillcom>
2025-03-03 15:47:27 +0700

    list-set-map

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Main {
+    static boolean compare(int a, int b) {
+        return a > b;
+    }
+
+    public static void main(String[] args) {
+        /*
+        List: Danh sách có thứ tự, cho phép trùng lặp (ví dụ: ArrayList, LinkedList).
+        Thực tế: Dùng để lưu danh sách nhân viên, đơn hàng trong ứng dụng.
+        Đặc điểm: Truy cập nhanh theo index (get(int index)), phù hợp khi cần duyệt tuần tự.
+
+        Set: Tập hợp không có thứ tự, không cho phép trùng lặp (ví dụ: HashSet, TreeSet).
+        Thực tế: Lưu danh sách ID unique hoặc email trong hệ thống.
+        Đặc điểm: TreeSet tự động sắp xếp, HashSet tối ưu tìm kiếm.
+
+        Map: Tập hợp key-value, không trùng key (ví dụ: HashMap, TreeMap).
+        Thực tế: Lưu cấu hình (key = "db.url", value = "jdbc:mysql://...") hoặc cache dữ liệu.
+        Đặc điểm: Truy xuất nhanh qua key (get(key)).
+        */
+
+        List<Student> studentList = new ArrayList<>();
+        Set<String> studentIds = new HashSet<>();
+        Map<String, Double> studentScores = new HashMap<>();
+
+        addStudent(studentList, studentIds, studentScores, "1", "hieupv", 22, 5.0);
+        addStudent(studentList, studentIds, studentScores, "2", "hieupv", 22, 5.0);
+        addStudent(studentList, studentIds, studentScores, "3", "hieupv", 22, 5.0);
+        printStudentInfo(studentList, studentScores);
+
+    }
+
+    private static void addStudent(List<Student> studentList, Set<String> studentIds, Map<String, Double> studentScores, String id, String name, int age, double score) {
+        if (studentIds.contains(id)) {
+            System.out.println("ID " + id + " already exists!");
+            return;
+        }
+
+        Student student = new Student(id, name, age);
+        studentList.add(student);
+        studentIds.add(id);
+        studentScores.put(id, score);
+    }
+
+    public static void printStudentInfo(List<Student> studentList, Map<String, Double> studentScores) {
+        for (Student student : studentList) {
+            System.out.println(student + ", Score: " + studentScores.get(student.getId()));
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+
+    Student(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age;
+    }
+}
+
